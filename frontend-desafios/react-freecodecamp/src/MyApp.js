62cf19901752a7b3aec6c4863be94934bc6deab4
ebraimcarvalho <ebraimcarvalho@gmail.com>
2019-12-26 03:22:17 -0300

    styles inline with jsx and back js

--- frontend-desafios/react-freecodecamp/src/MyApp.js
+++ frontend-desafios/react-freecodecamp/src/MyApp.js
@@ -17,11 +17,26 @@ import './style.css'
 // }
 
 function MyApp() {
-  const firstName = "Ebraim";
-  const lastName = "Carvalho";
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay
+  const styles = {
+    fontSize: 30
+  }
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+    styles.color = "#8475Df"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+    styles.color = "#500500"
+  } else {
+    timeOfDay = "night";
+    styles.color = "#005"
+  }
 
   return(
-    <h1>Hello, {`${firstName} ${lastName}, tudo beleza?`}</h1>
+  <h1 style={styles}>Good {timeOfDay}</h1>
   )
 }
 
