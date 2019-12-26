f6076577dc96feb0d819c0a02d94c9918c867ccd
ebraimcarvalho <ebraimcarvalho@gmail.com>
2019-12-26 00:56:54 -0300

    create MyInfo - second exercise

--- frontend-desafios/react-freecodecamp/src/index.js
+++ frontend-desafios/react-freecodecamp/src/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function MyInfo() {
+  return (
+    <div>
+      <h1>Ebraim Carvalho</h1>
+      <p>I am learning React, and I want visit:</p>
+      <ul>
+        <li>Netherlands</li>
+        <li>Portugal</li>
+        <li>Canada</li>
+      </ul>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <div>
-    <h1>Hey</h1>
-    <ul>
-      <li>New York</li>
-      <li>New York</li>
-      <li>New York</li>
-    </ul>
-  </div>, 
-  document.getElementById('root')
-);
+  <MyInfo />,
+  document.getElementById("root")
+)
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
