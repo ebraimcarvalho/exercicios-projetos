c47cfa1bda70e25bdead9da9d23e8817c5ae5cda
ebraimcarvalho <ebraimcarvalho@gmail.com>
2019-12-26 02:34:37 -0300

    create 4 checkbox with paragraph next them

--- frontend-desafios/react-freecodecamp/src/MyApp.js
+++ frontend-desafios/react-freecodecamp/src/MyApp.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React from 'react'
 
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+// import Header from './Header';
+// import MainContent from './MainContent';
+// import Footer from './Footer';
 
-function MyApp() {
+function MyApp () {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <input type="checkbox"/>
+      <p>Fazer Café</p>
+
+      <input type="checkbox"/>
+      <p>Fazer Feira</p>
+
+      <input type="checkbox"/>
+      <p>Estudar</p>
+
+      <input type="checkbox"/>
+      <p>Trabalhar</p>
     </div>
   )
 }
 
-export default MyApp;
+export default MyApp
--- frontend-desafios/react-freecodecamp/src/index.js
+++ frontend-desafios/react-freecodecamp/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 // import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import MyApp from './MyApp'
+import MyApp from "./MyApp"
 
 ReactDOM.render(
   <MyApp />,
