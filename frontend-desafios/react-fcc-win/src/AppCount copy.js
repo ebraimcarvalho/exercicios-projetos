839be1683fc10b8f79e1c71e6fb58fa4044687ca
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2019-12-31 15:28:13 -0300

    AppCount increase and decrease

---
+++ frontend-desafios/react-fcc-win/src/AppCount copy.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+    this.setCountPlus = this.setCountPlus.bind(this)
+    this.setCountMinus = this.setCountMinus.bind(this)
+  }
+
+  setCountPlus() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  }
+
+  setCountMinus() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count - 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.setCountPlus}>Increase</button>
+        <button onClick={this.setCountMinus}>Decrease</button>
+      </div>
+    )    
+  }
+}
+
+export default App
