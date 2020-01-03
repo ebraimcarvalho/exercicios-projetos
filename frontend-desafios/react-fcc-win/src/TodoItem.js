99c5ae22a4b7a330d21295b7ab6747d2299bc3dd
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2020-01-03 18:23:18 -0300

    style todo completed

--- frontend-desafios/react-fcc-win/src/TodoItem.js
+++ frontend-desafios/react-fcc-win/src/TodoItem.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 function TodoItem(props) {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#cdcdcd",
+    textDecoration: "line-through"
+  }
+
   return (
     <div className="todo-item">
       <input 
@@ -8,7 +14,7 @@ function TodoItem(props) {
         checked={props.data.completed}
         onChange={() => props.handleChange(props.data.id)}
       />
-      <p>{props.data.text}</p>
+      <p style={props.data.completed ? completedStyle : null}>{props.data.text}</p>
     </div>
   )
 }
--- frontend-desafios/react-fcc-win/src/index.js
+++ frontend-desafios/react-fcc-win/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom'
 import './style.css';
 import * as serviceWorker from './serviceWorker';
 
-import App from './App'
+import Todo from './Todo'
 
 ReactDOM.render(
-  <App />,
+  <Todo />,
   document.getElementById('root')
 )
 
