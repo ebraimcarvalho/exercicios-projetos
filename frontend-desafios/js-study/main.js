508dcf271cc87849fdb34816d260b5133bd12b9d
ebraimcarvalho <ebraimcarvalho@gmail.com>
2020-01-14 13:45:28 -0300

    start js path study

---
+++ frontend-desafios/js-study/main.js
@@ -0,0 +1,29 @@
+// function howMany(selectObject) {
+//   let numberSelected = 0;
+//   for (let i = 0; i < selectObject.options.length; i++) {
+//     if (selectObject.options[i].selected) {
+//       numberSelected++;
+//     }
+//   }
+//   return numberSelected;
+// }
+
+// let btn = document.getElementById('btn');
+// btn.addEventListener('click', function() {
+//   alert('Number of options selected: ' + howMany(document.selectForm.musicTypes) + ', sacou peixe?');
+// });
+
+function howMany(selectObject) {
+  let numberSelected = 0;
+  for (let i = 0; i < selectObject.options.length; i++) {
+    if (selectObject.options[i].selected) {
+      numberSelected ++
+    }
+  }
+  return numberSelected;
+}
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function() {
+  alert('Number of options selected: ' + howMany(document.selectForm.musicTypes))
+});
