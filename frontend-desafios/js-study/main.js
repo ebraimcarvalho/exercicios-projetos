bcaf57355875919ee68ac63844b4cf4a11b0de2a
ebraimcarvalho <ebraimcarvalho@gmail.com>
2020-01-15 18:48:35 -0300

    training factorial

--- frontend-desafios/js-study/main.js
+++ frontend-desafios/js-study/main.js
@@ -16,14 +16,31 @@
 function howMany(selectObject) {
   let numberSelected = 0;
   for (let i = 0; i < selectObject.options.length; i++) {
-    if (selectObject.options[i].selected) {
-      numberSelected ++
+    if (selectObject[i].selected) {
+      numberSelected++;
     }
   }
   return numberSelected;
 }
 
 let btn = document.getElementById('btn');
-btn.addEventListener('click', function() {
-  alert('Number of options selected: ' + howMany(document.selectForm.musicTypes))
-});
+btn.addEventListener('click', function () {
+  alert('Number of options selected: ' + howMany(document.getElementById('musicTypes')))
+})
+
+// const factorial = function fac(n) { return n < 2 ? 1 : n * fac(n - 1) }
+
+// console.log(factorial(4))
+
+var facty = 4*3*2*1 
+console.log(facty)
+
+function factorial(n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(4));
