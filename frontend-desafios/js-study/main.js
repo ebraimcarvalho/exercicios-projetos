1b2c0d51c8513abf5142f314df8a39563398729e
ebraimcarvalho <ebraimcarvalho@gmail.com>
2020-01-23 01:53:57 -0300

    style.background via DOM

--- frontend-desafios/js-study/main.js
+++ frontend-desafios/js-study/main.js
@@ -68,5 +68,29 @@ myObj['']               = 'Even an empty string';
 
 console.log(myObj);
 
-const body = document.getElementsByTagName('body');
-body.addEventListener('on load', document.write('Hello JavaScript, I\'ll the master ninja JS Dev!'))
+const bodys = document.getElementsByTagName('body');
+bodys.addEventListener('on load', document.write('Hello JavaScript, I\'ll the master ninja JS Dev! By the way, do you want a cookie?'))
+
+function change() {
+  // document.getElementsByTagName("H2") returns a NodeList of the <h2>
+  // elements in the document, and the first is number 0:
+  
+  var header = document.getElementsByTagName('H2').item(0);
+  // the firstChild of the header is a Text node:
+  header.firstChild.data = "A dynamic document 2";
+  // now the header is "A dynamic document".
+  
+  var para = document.getElementsByTagName("P").item(3);
+  para.firstChild.data = "This is the first paragraph.";
+  para.style.background = "rgb(255,0,0)";
+  
+  // create a new Text node for the second paragraph
+  var newText = document.createTextNode("This is the second paragraph.");
+  // create a new Element to be the second paragraph
+  var newElement = document.createElement("P");
+  // put the text in the paragraph
+  newElement.appendChild(newText);
+  // and put the paragraph on the end of the document by appending it to
+  // the BODY (which is the parent of para)
+  para.parentNode.appendChild(newElement);
+}
