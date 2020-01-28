eb482794da749570c851a15e80b54404806d7c16
ebraimcarvalho <ebraimcarvalho@gmail.com>
2020-01-27 23:22:38 -0300

    firstchild.data and style.background

--- frontend-desafios/js-study/main.js
+++ frontend-desafios/js-study/main.js
@@ -13,47 +13,50 @@ btn.addEventListener('click', function () {
   alert('Number of options selected: ' + howMany(document.getElementById('musicTypes')))
 })
 
-// const factorial = function fac(n) { return n < 2 ? 1 : n * fac(n - 1) }
+// Factorial
+const factorial = function fac(n) { return n < 2 ? 1 : n * fac(n - 1) }
 
-// console.log(factorial(4))
+console.log(factorial(4))
 
 var facty = 4*3*2*1 
 console.log(facty)
 
-function factorial(n) {
+function factorial2(n) {
   if (n == 0) {
     return 1;
   } else {
-    return n * factorial(n - 1);
+    return n * factorial2(n - 1);
   }
 }
 
-console.log(factorial(4));
+console.log("Factorial de 4 é " + factorial2(4));
 
-let myArray = ['1', '2', '3', '4', '5'];
-console.log(myArray.slice(1, 5));
+// Array and methods
+let myArray = ['1', '2', '3', '4', '5', '1', '2'];
+console.log(myArray.slice(1, 6)); // start index 1 and finish index 6 [2,3,4,5,1]
 
-// let a = [1, 2, 3]
-// a.map(function(element) { console.log(element*3) })
+let a = [1, 2, 3]
+a.map(function(element) { console.log(element*3) }) //[3,6,9]
 
-// let a1 = ['a', 10, 'b', 20, 'c', 30, 5]
-// let a2 = a1.filter(function(item) { return typeof item === 'number'; })
-// console.log(a2)  // logs [10, 20, 30]
+let a1 = ['a', 10, 'b', 20, 'c', 30, 5]
+let a2 = a1.filter(function(item) { return typeof item === 'number'; })
+console.log(a2)  // logs [10, 20, 30, 5]
 
 function isNumber(value) {
   return typeof value === 'number'
 }
-let a1 = [1, 2, 3]
-console.log(a1.some(isNumber))  // logs true
-let a2 = [1, '2', 3]
-console.log(a2.some(isNumber))  // logs true
-let a3 = ['1', '2', '3']
-console.log(a3.some(isNumber))  // logs false
+let a3 = [1, 2, 3]
+console.log(a3.some(isNumber))  // logs true
+let a4 = [1, '2', 3]
+console.log(a4.some(isNumber))  // logs true
+let a5 = ['1', '2', '3']
+console.log(a5.some(isNumber))  // logs false
 
-let a = [11, 20, 30]
-let total = a.reduce(function(accumulator, currentValue) { return accumulator + currentValue })
-console.log(total) // Prints 60
+let a6 = [11, 20, 30]
+let total = a6.reduce(function(accumulator, currentValue) { return accumulator + currentValue })
+console.log(total) // Prints 61
 
+// Object and dot notation
 var myObj = new Object(),
     str = 'myString',
     rand = Math.random(),
@@ -69,7 +72,7 @@ myObj['']               = 'Even an empty string';
 console.log(myObj);
 
 const bodys = document.getElementsByTagName('body');
-bodys.addEventListener('on load', document.write('Hello JavaScript, I\'ll the master ninja JS Dev! By the way, do you want a cookie?'))
+bodys.addEventListener('on load', document.write('Hello JavaScript, I\'ll the master ninja JS Dev! By the way, do you want a cookie? this was inserted by main.js document.write'))
 
 function change() {
   // document.getElementsByTagName("H2") returns a NodeList of the <h2>
